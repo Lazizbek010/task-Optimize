0ecfb7b1ace7a68682a844c8154377ff668fe80b
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2023-03-08 09:19:28 +0500

    sel set

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const telNumber = document.querySelector('.tel-number')
 const openTel = document.querySelector('.open-tel')
 const searchSel = document.querySelector('.search__sel')
 const searchOptions = document.querySelector('.search__sel__options')
+const resLangSel = document.querySelector('.res-lang-sel')
+const resLangOptions = document.querySelector('.res-lang__options')
+
+
 let open = true
 
 openTel.addEventListener('click', ()=>{
@@ -17,4 +21,7 @@ openTel.addEventListener('click', ()=>{
 searchSel.addEventListener('click', ()=>{
     searchOptions.classList.toggle('block')
 })
-console.log(searchOptions);
+
+resLangSel.addEventListener('click', ()=>{
+    resLangOptions.classList.toggle('block')
+})
